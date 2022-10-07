b0340d9c20d55b5b0cb3686be5db9624a1517fe1
networkabnormal <108947393+networkabnormal@users.noreply.github.com>
2022-10-07 20:01:09 +0800

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,7 +10,7 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string_view addressPrefix = "ALC";
+    const std::string_view addressPrefix = "AC";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
     const std::string ticker = "ALC";
